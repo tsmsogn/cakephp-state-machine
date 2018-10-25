8783e4399e0a2ac50a4fa77c5d25807cc2cd5564
tsmsogn <tsmsogn@gmail.com>
2018-10-25 17:44:45 +0900

    Add a test for toDot()

--- tests/TestCase/StateMachine/Model/Behavior/StateMachineBehaviorTest.php
+++ tests/TestCase/StateMachine/Model/Behavior/StateMachineBehaviorTest.php
@@ -608,7 +608,30 @@ class StateMachineBehaviorTest extends TestCase
 
     public function testToDot()
     {
-        $this->Vehicle->toDot();
+        $expected = <<<EOT
+digraph finite_state_machine {
+	rankdir=LR
+	fontsize=12
+	node [shape = circle];
+	parked -> idling [ label = "ignite" ];
+	stalled -> stalled [ label = "ignite" ];
+	idling -> parked [ label = "park" ];
+	first_gear -> parked [ label = "park" ];
+	idling -> first_gear [ label = "shift_up" ];
+	first_gear -> second_gear [ label = "shift_up" ];
+	second_gear -> third_gear [ label = "shift_up" ];
+	first_gear -> idling [ label = "shift_down" ];
+	second_gear -> first_gear [ label = "shift_down" ];
+	third_gear -> second_gear [ label = "shift_down" ];
+	first_gear -> stalled [ label = "crash" ];
+	second_gear -> stalled [ label = "crash" ];
+	third_gear -> stalled [ label = "crash" ];
+	stalled -> parked [ label = "repair" ];
+	first_gear -> idling [ label = "idle" ];
+	all -> parked [ label = "turn_off" ];
+}
+EOT;
+        $this->assertEquals($expected , $this->Vehicle->toDot());
     }
 
     public function testCreateDotFileForRoles()
